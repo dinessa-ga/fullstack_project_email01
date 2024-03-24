32d93d27299f4725537c9b47dc7577f2a851f6c9
dinessa-ga <dinessa.ga@gmail.com>
2024-03-24 10:36:12 -0500

    feat: creación de nueva rama para la api y creación de serializers

---
+++ README.md
@@ -0,0 +1,1 @@
+Proyecto de plantilla de correo electrónico
--- prueba_api/myapp/serializers.py
+++ prueba_api/myapp/serializers.py
@@ -1,1 +1,12 @@
 from rest_framework import serializers
+from .models import EmailTemplate, SMTPConfiguration
+
+class EmailTemplateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmailTemplate
+        fields = ('id', 'subject', 'content', 'header_image','footer_image' )
+
+class SMTPConfigurationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SMTPConfiguration
+        fields = ('id','smtp_server','port','smtp_user','smtp_password', 'recipient_email' )
