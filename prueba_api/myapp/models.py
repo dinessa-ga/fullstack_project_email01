da54f3572e9637ec8f670514d8a64ef977e1367d
dinessa-ga <dinessa.ga@gmail.com>
2024-03-24 09:33:04 -0500

    feat: creando modelos

--- prueba_api/myapp/models.py
+++ prueba_api/myapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class EmailTemplate(models.Model):
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
+    header_image = models.ImageField(upload_to='email_headers/')
+    footer_image = models.ImageField(upload_to='email_footers/')
+
+    def __str__(self):
+        return self.subject
+    
+class SMTPConfiguration(models.Model):
+    smtp_server = models.CharField(max_length=200)
+    port = models.IntegerField()
+    smtp_user = models.CharField(max_length=200)
+    smtp_password = models.CharField(max_length=200)
+    recipient_email = models.EmailField()
+
+def __str__(self):
+        return self.smtp_server
